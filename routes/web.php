e88f7d0efbb2387f0c56b3854f807ed922335bd7
DESKTOP-O15N8BP\shafly <shaflynaufal96@gmail.com>
2017-06-15 15:49:36 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/customer', function () {
+    return view('customer/pages/welcome');
+}
 
 Route::get('/', function () {
-<<<<<<< HEAD
-    return view('customer/master/master');
-=======
+
     $data['active'] = "";
     $data['active2'] = "";
 
@@ -23,29 +24,17 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'products'],function () {
   Route::get('/form', 'productController@showform');
-  Route::post('/form','productController@addproduct');
   Route::get('/tabel', 'productController@showtable');
-  Route::get('/delete/{id}', 'productController@delete');
 
 });
 Route::group(['prefix' => 'news'],function () {
   Route::get('/form', 'newsController@showform');
-  Route::post('/form','newsController@addnews');
   Route::get('/tabel', 'newsController@showtable');
-  Route::get('/update/{id}', 'newsController@showupdate');
-  Route::post('/update/{id}', 'newsController@update');
-  Route::get('/delete/{id}', 'newsController@delete');
 
-  Route::get('/test/{id}','newsController@test');
 });
 
 Route::group(['prefix' => 'page-editor'],function () {
   Route::get('/homepage', 'pagesController@showhome');
   Route::get('/customer-list', 'pagesController@showcustomer');
   Route::get('/customer-add', 'pagesController@addcustomer');
-  Route::get('/contacts', 'pagesController@contacts');
-
-
-
->>>>>>> e3a15615dd41f518ac28b0de0ed6488c0f5d328f
 });
