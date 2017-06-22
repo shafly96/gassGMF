2450bb1b413148a4ec35ca6822c048796786b8a0
Ario Bimo <hariyosoariobimo@gmail.com>
2017-06-22 14:05:43 +0700

    product dah gak dropdown

--- storage/framework/views/ef1347775ef1728590f97b0b6351e4134eaaaf5d.php
+++ storage/framework/views/ef1347775ef1728590f97b0b6351e4134eaaaf5d.php
@@ -8,12 +8,20 @@ Product
 <?php $__env->startSection('content'); ?>
 <div class="col-md-12 rata" style="margin-top: 50px">
 	<div class="col-md-7">
-		<div class="col-md-12" style="padding-right: 60px; padding-bottom: 60px">
-			<img src="<?php echo e(url('')); ?>/images/product/<?php echo e($product->filename); ?>" style="width: 100%; height:auto " class="img-responsive">
+		<div class="col-md-12" style="padding-left: 30px; padding-right: 30px; padding-bottom: 60px">
+			<div class="w3-content w3-section" style="width:100%; height: 200px; text-align:center">
+				<?php $__currentLoopData = $product; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key=>$value): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+				<img class="mySlides" src="<?php echo e(url('')); ?>/images/product/<?php echo e($product[$key]->filename); ?>" style="height: 100%; margin: 0 auto">
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+
+				<button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+				<button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+			</div>
 		</div>
 		<div class="col-md-12">
 			<p class="judul-berita" style="font-size: 20px">Description</p>
-			<p class="paragraf"><?php echo e($product->product_description); ?></p>
+			<p class="paragraf"><?php echo e($product[0]->product_description); ?></p>
 
 			<p class="judul-berita" style="margin-top: 50px; font-size: 20px">Review</p>
 			<?php if(isset($review[0])): ?>
@@ -43,7 +51,7 @@ Product
 					</div>
 					<div class="form-group row">
 						<div class="col-xs-12">
-							<input type="hidden" name="id" value="<?php echo e($product->product_id); ?>">
+							<input type="hidden" name="id" value="<?php echo e($product[0]->product_id); ?>">
 							<?php echo e(csrf_field()); ?>
 
 							<input type="submit" class="btn btn-primary" value="Send">
@@ -56,21 +64,44 @@ Product
 	<div class="col-md-5">
 		<p class="judul-berita">Technical Specification</p>
 		<p class="paragraf">
-			<?php print $product->product_specification; ?>
+			<?php print $product[0]->product_specification; ?>
 		</p>
 	</div>
 </div>
 <script>
-	// $("#description").click(function(){
-	// 	$(".review").hide();
-	// 	$(".description").show();
-	// });
+	var myIndex = 0;
+	carousel();
 
-	// $("#review").click(function(){
-	// 	$(".description").hide();
-	// 	$(".review").show();
-	// });
-</script>
-<?php $__env->stopSection(); ?>
+	function carousel() {
+		var i;
+		var x = document.getElementsByClassName("mySlides");
+		for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";  
+		}
+		myIndex++;
+		if (myIndex > x.length) {myIndex = 1}    
+			x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n > x.length) {slideIndex = 1}    
+		if (n < 1) {slideIndex = x.length}
+			for (i = 0; i < x.length; i++) {
+				x[i].style.display = "none";  
+			}
+			x[slideIndex-1].style.display = "block";  
+		}
+	</script>
+	<?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('customer.master.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
